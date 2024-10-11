af2625d0c4a4bf240616cd2858fa011ac73c3320
shahbuland <smatiana1@gmail.com>
2024-10-11 22:40:13 +0000

    better defaults, add saving, fix coco to have labels

--- dit/configs.py
+++ dit/configs.py
@@ -4,30 +4,31 @@ from typing import Optional, Dict
 @dataclass
 class ModelConfig:
     n_layers : int = 12
-    n_heads : int = 6
-    d_model : int = 384
-    image_size : int = 64
+    n_heads : int = 12
+    d_model : int = 768
+    image_size : int = 512
     sample_size : int = 64
-    channels : int = 3
+    channels : int = 4
     patch_size : int = 4
-    use_vae = False
-    flash : bool = False
+    use_vae = True
+    flash : bool = True
     take_label : bool = True # Take the batch as (pixel_values, label_str) instead of pixel_values
     cfg_prob : float = 0.1
 
 @dataclass
 class TrainConfig:
-    dataset : str = "mnist"
-    target_batch_size : int = 64
-    batch_size : int = 16
+    dataset : str = "coco"
+    target_batch_size : int = 256
+    batch_size : int = 128
     epochs : int = 100
     # optimizer
     opt : str = "AdamW"
     opt_kwargs : Dict = field(default_factory = lambda : {
         "lr": 1.0e-3,
-        "eps": 1e-7,
+        "eps": 1.0e-15,
         "betas" : (0.9, 0.96),
-        "weight_decay" : 0.0
+        "weight_decay" : 0.00,
+        #"precondition_frequency" : 50
     })
 
     # scheduler
@@ -38,25 +39,20 @@ class TrainConfig:
         "eta_min" : 1.0e-6
     })
 
+    checkpoint_root_dir = "checkpoints"
+
     log_interval : int = 1
     sample_interval : int = 50
-    n_samples : int = 8 # Number of samples to log each time (too many gets crowded)
-    sample_prompts = [
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight"
-    ]
+    save_interval : int = 2500
+    resume : bool = True
+
+    n_samples : int = 4 # Number of samples to log each time (too many gets crowded)
+    sample_prompts = ["a dog in a park", "the blue sky", "the ocean", "the beach"]
     grad_clip : float = -1 # Clip grad norms to this value
     normalize_every : int = 1
     
-
 @dataclass
 class LoggingConfig:
-    run_name : str = "mnist 40M (+ngpt +1e-3 lr)"
+    run_name : str = "coco 150M (adam, +ngpt, lr=1e-3)"
     wandb_entity : str = "shahbuland"
     wandb_project : str = "mnist_sanity"

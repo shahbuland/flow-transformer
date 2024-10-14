ee3f8524e83a093cacf1960cfc4c284b1c40387b
shahbuland <smatiana1@gmail.com>
2024-10-14 02:52:55 +0000

    add val loader to train

--- train.py
+++ train.py
@@ -27,4 +27,12 @@ if __name__ == "__main__":
         deterministic=True  # This will use a fixed seed internally
     )
 
-    trainer.train(model, train_loader)
+    val_loader = create_loader(
+        dataset_name=train_cfg.dataset,
+        batch_size=train_cfg.batch_size * train_cfg.val_batch_mult,
+        image_size=model_cfg.image_size,
+        deterministic=True,
+        split='validation'
+    ) if train_cfg.val_interval != -1 else None
+
+    trainer.train(model, train_loader, val_loader)
